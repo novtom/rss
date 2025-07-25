9ab1640db72f699d2ac37bea93fcc3209a3714b8
novtom <novtom@gmail.com>
2025-07-25 08:12:05 +0200

    Update generate_feed.py

--- .github/generate_feed.py
+++ .github/generate_feed.py
@@ -4,7 +4,7 @@ import re
 
 podcasts = {
     "pro-a-proti.xml": "https://api.mujrozhlas.cz/rss/podcast/0bc5da25-f081-33b6-94a3-3181435cc0a0.rss",
-    "nazory-argumenty.xml": "https://api.mujrozhlas.cz/rss/podcast/f4133d64-ccb2-30e7-a70f-23e9c54d8e12.rss",
+    "nazory-argumenty.xml": "https://api.mujrozhlas.cz/rss/podcast/f4133d64-ccb2-30e7-a70f-23e9c54d8e76.rss",
 }
 
 os.makedirs("feeds", exist_ok=True)
